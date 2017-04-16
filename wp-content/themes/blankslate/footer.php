5b90f206117a9826182d56af6a9898b48edd8f5a
Tukiyo Hijiri <andoromeda92@gmail.com>
2017-04-16 08:12:36 +0000

    c9 test

---
+++ wp-content/themes/blankslate/footer.php
@@ -0,0 +1,11 @@
+<div class="clear"></div>
+</div>
+<footer id="footer" role="contentinfo">
+<div id="copyright">
+<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme Based By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?> & Costum By:tukiyo92
+</div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
