ad485726fba636925b837f2aa15b0b56b32df267
Tukiyo Hijiri <andoromeda92@gmail.com>
2017-05-31 22:30:32 +0000

    headerImg

--- wp-content/themes/blankslate/footer.php
+++ wp-content/themes/blankslate/footer.php
@@ -1,11 +1,11 @@
 <div class="clear"></div>
 </div>
+</div><!-- wrapper閉じ -->
 <footer id="footer" role="contentinfo">
 <div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme Based By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?> & Costum By:tukiyo92
+<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme Based By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?> & Costum By:nakai
 </div>
 </footer>
-</div>
 <?php wp_footer(); ?>
 </body>
 </html>
